3ee9a58ad26a82dfe1229f7f0b8c97a622d84f83
Kieran Browne <kierantbrowne@gmail.com>
2019-05-18 00:14:11 +1000

    working

--- index.js
+++ index.js
@@ -7764,11 +7764,6 @@ var author$project$Main$view = function (model) {
 					_List_Nil,
 					_List_fromArray(
 						[
-							elm$html$Html$text(
-							elm$core$Debug$toString(
-								elm$core$Array$length(model.options))),
-							elm$html$Html$text(
-							A2(author$project$Main$similar, model, model)),
 							A2(
 							elm$html$Html$div,
 							_List_Nil,
